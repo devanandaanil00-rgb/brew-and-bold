8f7430f223026eef8c101d5d1322d7af26adfbb9
devanandaanil00-rgb <devanandaanil.00@gmail.com>
2026-03-30 23:09:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,4 +18,5 @@ app.get('/api/menu', async (req, res) => {
 });
 
 // Start Server
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT =process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
