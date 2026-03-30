b43cfb90a6634fd588bc9146492ddedc307f2199
devanandaanil00-rgb <devanandaanil.00@gmail.com>
2026-03-30 22:56:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.static('public')); // Serves your HTML
 
 // Connect to MongoDB Compass (Localhost)
-mongoose.connect('mongodb://localhost:27017/brewBoldDB');
+mongoose.connect(process.env.MONGO_URI);
 
 // API to get menu items
 app.get('/api/menu', async (req, res) => {
