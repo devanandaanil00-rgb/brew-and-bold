54d7f2ba0d34ebcdcb223b2996b2872fa7a5baab
devanandaanil00-rgb <devanandaanil.00@gmail.com>
2026-03-31 11:17:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const Menu = require('./models/menu);
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Menu = require("./models/menu");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static('public')); // Serves your HTML
+app.use(express.static("public")); // Serves your HTML
 
 // Connect to MongoDB Compass (Localhost)
 mongoose.connect(process.env.MONGO_URI);
 
 // API to get menu items
-app.get('/api/menu', async (req, res) => {
-    const Menu=await Menu.find();
+app.get("/api/menu", async (req, res) => {
+    const Menu=await menu.find();
     res.json(menu);
 });
 
