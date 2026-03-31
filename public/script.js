2a5605d83663985c14e7a6afefdd3425b17c1ec3
devanandaanil00-rgb <devanandaanil.00@gmail.com>
2026-03-31 13:11:27 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,8 +8,27 @@ async function fetchMenu() {
             <h4>${item.name}</h4>
             <p>${item.description}</p>
             <strong>$${item.price}</strong>
+            <br>
+            <button onclick="orderItem('${item.name}',
+            '${item.price}')">
+             order
+            </button>
         </div>
     `).join('');
 }
 
 fetchMenu();
+
+function orderItem(name, price) {
+  fetch('/api/order', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, price })
+  })
+  .then(res => res.json())
+  .then(data => {
+    alert("Order placed for " + name);
+  });
+}
